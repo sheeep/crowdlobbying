d1ada5e9626e8469c55f40f7f8d5a0d40611ddd8
Pascal Wacker <pascal.wacker@gmx.ch>
2019-05-26 19:45:28 +0200

    redirect so single campaign

--- www/src/Controller/DefaultController.php
+++ www/src/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Campaign;
 use App\Repository\CampaignRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -11,11 +13,18 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default_index", methods={"GET"})
      */
-    public function index(CampaignRepository $campaignRepository)
+    public function index(CampaignRepository $campaignRepository, Request $request)
     {
+        $campaigns = $campaignRepository->findActiveCampaigns();
+        if (count($campaigns) == 1) {
+            /** @var Campaign $campaign */
+            $campaign = array_shift($campaigns);
+
+            return $this->redirect('https://' . $campaign->getSlug() . '.' . $request->getHost());
+        }
 
         return $this->render('default/index.html.twig', [
-            'campaigns' => $campaignRepository->findActiveCampaigns(),
+            'campaigns' => $campaigns,
         ]);
     }
 }
