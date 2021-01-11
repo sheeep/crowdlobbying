71e57ff6fb90579349b8b9dcc591296e37174f0e
David Greminger <david.greminger@1up.io>
2021-01-11 16:14:28 +0100

    Fix CS

--- www/src/Migrations/Version20210111145702.php
+++ www/src/Migrations/Version20210111145702.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210111123440 extends AbstractMigration
+final class Version20210111145702 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,7 +22,9 @@ final class Version20210111123440 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE politician CHANGE last_name lastname VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE person_argument (id INT AUTO_INCREMENT NOT NULL, campaign_id INT NOT NULL, person_id INT NOT NULL, argument LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_99579E3DF639F774 (campaign_id), INDEX IDX_99579E3D217BBB47 (person_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE person_argument ADD CONSTRAINT FK_99579E3DF639F774 FOREIGN KEY (campaign_id) REFERENCES campaign (id)');
+        $this->addSql('ALTER TABLE person_argument ADD CONSTRAINT FK_99579E3D217BBB47 FOREIGN KEY (person_id) REFERENCES person (id)');
     }
 
     public function down(Schema $schema): void
@@ -30,6 +32,6 @@ final class Version20210111123440 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE politician CHANGE lastname last_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP TABLE person_argument');
     }
 }
