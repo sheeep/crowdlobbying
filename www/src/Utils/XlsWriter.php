18c61244d47f801be0e771efc43bafd16727403c
David Greminger <david.greminger@1up.io>
2021-01-13 11:07:38 +0100

    Reverte change from feature/custom-argument

--- www/src/Utils/XlsWriter.php
+++ www/src/Utils/XlsWriter.php
@@ -7,7 +7,6 @@ namespace App\Utils;
 use App\Entity\Argument;
 use App\Entity\Campaign;
 use App\Entity\Person;
-use App\Entity\PersonArgument;
 use App\Entity\Politician;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
@@ -66,9 +65,6 @@ class XlsWriter implements WriterInterface
             /** @var Argument $argument */
             $argument = $campaignEntry->getArgument();
 
-            /** @var PersonArgument $personArgument */
-            $personArgument = $campaignEntry->getPersonArgument();
-
             $data[] = [
                 $campaignEntry->getId(),
                 $person->getLastname(),
@@ -81,7 +77,7 @@ class XlsWriter implements WriterInterface
                 $person->getUpdatedAt()->format('d.m.Y H:i:s'),
                 $politician->getLastname(),
                 $politician->getName(),
-                $argument ? (string) $argument->getArgument() : (string) $personArgument->getArgument(),
+                $argument->getArgument(),
                 $campaignEntry->getCreatedAt()->format('d.m.Y H:i:s'),
                 $campaignEntry->getUpdatedAt()->format('d.m.Y H:i:s'),
             ];
